174785a93786e12d971cca47fc78c827c63ea5b1
Sajal Jain <jsajal1993@gmail.com>
2019-02-24 20:47:04 -0600

    Added implementation of generatecandidates function

--- Project1/src/extract.py
+++ Project1/src/extract.py
@@ -3,11 +3,12 @@ import ml
 
 # candidate of all sub strings
 class candidate:
-    def __init__(self, position,feature1,feature2):
+    def __init__(self, text, position, feature1, feature2):
+        self.text = text
         # position is a array of int
         # for example "Elon Musk" in "Elon Musk has three wives" will have position [0,1]
         # for example "wives" in "Elon Musk has three wives" will have position [4]
-        self.position = position
+        self.label = position
 
         # TODO: define a set of features (e.g. how many substring there are, whether initial capital...)
         self.features1 = feature1
@@ -20,9 +21,10 @@ def readfile():
   texts = []
   labels = []
   # read files from I
-  for i in range(1, 59):
+  for i in range(1, 2):
     file = str(i).zfill(3)
     f = open("../I/" + file + "l.txt", "r", encoding='utf-8-sig')
+    print("../I/" + file + "l.txt")
 
     # text is the sub strings of file splited by space
     # label is the position of name
@@ -39,6 +41,8 @@ def readfile():
         label.append(idx)
         # delete <name> tag
         text[idx] = text[idx][6:len(text[idx])-7]
+      else:
+        label.append(-1) #indicates that this word is neither a name indiviually nor can it be a part of a name
 
     texts.append(text)
     labels.append(label)
@@ -50,10 +54,58 @@ def readfile():
 # return features of  "Elon, Musk, has, three, wives,
 # Elon Musk, Musk has, has three, three wives,
 # Elon Musk has, Musk has three, has three wives"
-def generatecandiates(texts):
+def generatecandiates(texts, labels):
   candidates = []
-  candidates.append(candidate([0],1,1))
-  candidates.append(candidate([0],1,1))
+
+  #k refers to different files in I
+  #generate candidates of length 1
+  for k in range(0,len(texts)):
+    for i in range(0,len(texts[k])):
+      text = texts[k][i]
+      candidates.append(candidate(text, labels[k][i], 1, 1))
+
+  #generate candidates of length 2
+  for k in range(0,len(texts)):
+    for i in range(0,len(texts[k]) - 1):
+      text = texts[k][i] + " " + texts[k][i+1]
+
+      if labels[k][i] != -1 and labels[k][i+1] != -1:
+        #this is a probable candidate name of length 2 words
+        if texts[k][i][-1:] == ',' :
+          # for strings like "Elon Musk, Mark Manson and Tom Sawyer are good friends.", candidates are generated as
+          # Elon Musk, ; Musk, Mark and so on; so we need to avoid combining two names together like in Musk, Mark;
+          # if a comma is encountered in probable candidate generation that indicates one name ended and another name started, then that 
+          # is marked as not a name
+          label = -1 
+        else:
+          label = labels[k][i]
+      else:
+        label = -1 #includes cases like "Musk has" or "said Elon" where one of the word is not a name and cases like "has three" where none of the word is a name
+
+      candidates.append(candidate(text, label, 1, 1))
+
+  #generate candidates of length 3
+  for k in range(0,len(texts)):
+    for i in range(0,len(texts[k]) - 2):
+      text = texts[k][i] + " " + texts[k][i+1] + " " + texts[k][i+2]
+
+      if labels[k][i] != -1 and labels[k][i+1] != -1 and labels[k][i+2] != -1:
+        #this is a probable candidate name of length 3 words
+        if texts[k][i][-1:] == ',' or texts[k][i+1][-1:] == ',':
+          # for strings like "Elon Musk, Mark Manson and Tom Sawyer are good friends.", candidates are generated as
+          # Elon Musk, Mark ; Musk, Mark Manson and so on; so we need to avoid combining two names together
+          # if a comma is encountered in probable candidate generation that indicates one name ended and another name started, then that 
+          # is marked as not a name
+          label = -1 
+        else:
+          label = labels[k][i]
+      else:
+        label = -1 #includes cases like Musk has or said Elon where one of the word is not a name and cases like has three where none of the word is a name
+
+      candidates.append(candidate(text, label, 1, 1))
+
+  #candidates.append(candidate([0],1,1))
+  #candidates.append(candidate([0],1,1))
   return candidates
 
 # TODO: preprocess all candidates (e.g. pruning rules)
@@ -74,9 +126,11 @@ def evaluate(candidates):
 def main():
   # read files from I
   texts,labels = readfile()
-  # print(texts)
-  # print(labels)
-  candidates = generatecandiates(texts)
+  #print(texts)
+  #print(labels)
+  candidates = generatecandiates(texts, labels)
+  #for i in range(0,len(candidates)):
+  #  print('{} {}'.format(candidates[i].text, candidates[i].label))
   candidates = preprocess(candidates)
   candidates = ml.traindata(candidates,labels)
   candidates = postprocess(candidates)
